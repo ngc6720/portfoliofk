4fff73e991892c9d72644ca7951980ff1dddcb64
ngc6720 <francoiskerforn@gmail.com>
2025-08-31 14:26:00 +0200

    progress bar width quick fix

--- core/animation/progress.js
+++ core/animation/progress.js
@@ -1,20 +1,20 @@
 function makeLowpass(_a = 0.92) {
+  let hist = 0;
 
-    let hist = 0;
-
-    return (val = 0, a = _a) => {
-
-        const result = val * (1 - a) + hist * a;
-        hist = result;
-        return result;
-    }
+  return (val = 0, a = _a) => {
+    const result = val * (1 - a) + hist * a;
+    hist = result;
+    return result;
+  };
 }
 
 const progressLowpass = makeLowpass();
 
 export default (app, progressEl, getPosition) => () => {
-    if (app.progressNeedsUpdate === false)
-        return;
-    requestAnimationFrame(app.animateProgress)
-    progressEl.style.setProperty('--multiplayerProgress', `${progressLowpass(getPosition()) * 100}%` )
-}
+  if (app.progressNeedsUpdate === false) return;
+  requestAnimationFrame(app.animateProgress);
+  progressEl.style.setProperty(
+    "--multiplayerProgress",
+    `${progressLowpass(getPosition() * 1.005) * 100}%`,
+  );
+};
